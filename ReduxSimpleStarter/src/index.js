d127d49469e34fda29de0993b2c0d4418b639fec
Anurag Patil <aspanurag@gmail.com>
2017-08-06 13:08:26 -0400

    Added Youtube API_KEY

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,21 @@
+//import React from 'react';
+const React = require('react'); //how to work with component
+const ReactDOM = require('react-dom');
+
+const API_KEY = "AIzaSyCEnfhD7D2nVyX7VM8GXpYom0mYMjJg70o";
+
+/*
+Create a new component. This component should 
+produce HTML
+JSX
+*/
+const App = () => {
+    return <div>Hi!!</div>;
+}
+
+/*
+Take this component's generarated HTML 
+and put it on the page (in the DOM)
+*/
+
+ReactDOM.render(<App />, document.querySelector('.container')); // Here < /> is used to instantiate the const App
