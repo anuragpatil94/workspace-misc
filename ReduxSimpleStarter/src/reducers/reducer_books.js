bcd9668e06837d5c0be97a3fb0ebffbc0ea781ce
Anurag Patil <aspanurag@gmail.com>
2017-08-10 19:43:19 -0400

    Book App- Completed Actions, Connected action to book-detail container

--- ReduxSimpleStarter/src/reducers/reducer_books.js
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'Harry Potter' },
-        { title: 'The Dark Tower' },
-        { title: 'Lord of Rings' },
-        { title: 'Game of Thrones' }
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 873 },
+        { title: 'The Dark Tower', pages: 456 },
+        { title: 'Lord of Rings', pages: 45 },
+        { title: 'Game of Thrones', pages: 1 }
     ]
 }
