e0f5078f94dd4189a9db1eb3851187645b1e5a0c
Anurag Patil <aspanurag@gmail.com>
2020-03-04 19:05:49 -0500

    Hover, Focus and Active Styles| Add Active through config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 // Customizing Tailwind
 module.exports = {
   theme: {
-    extend: {},
+    extend: {}
   },
-  variants: {},
-  plugins: [],
-}
+  variants: {
+    /* Here the order matters, initially active is not part of tailwind.css so we added it. 
+    but when we add a new variant to a css style we have to add all of them. 
+    Since hover comes before active hover should take precedence
+    */
+    backgroundColor: ["responsive", "hover", "focus", "active"]
+  },
+  plugins: []
+};
